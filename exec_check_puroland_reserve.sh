77baa54b5b5ca836a25b1927811da91241512123
NaohiroYoshida <y0shid0n1031@gmail.com>
2024-12-08 03:52:12 +0900

    パスを修正

--- exec_check_puroland_reserve.sh
+++ exec_check_puroland_reserve.sh
@@ -1,5 +1,4 @@
-# venvのdevelopをactivateしてから実行する
-source ~/py_venv/develop/bin/activate
-cd ~/Program/git_repo/line_bot_test
+# venvの環境をactivateしてから実行する
+source ~/.venv/puroland/bin/activate
+cd ~/git_repo/line_bot_test
 python check_puroland_reserve.py
-
